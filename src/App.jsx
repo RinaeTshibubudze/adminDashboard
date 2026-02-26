51845239138f43fcfea8fff844b7af9be3f8a241
Rinae Tshibubudze <vharhee@gmail.com>
2026-02-26 13:50:21 +0200

    Test the maping with string values

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import { useState } from "react";
 import Sidebar from "./components/Sidebar";
 
 const App = () => {
+  const [sidebar, setSidebarOpen] = useState(false);
+  const [activeTab, setActiveTab] = useState("overview");
   return (
     <div className="min-h-screen bg-gradient-to-r from-indigo-900 via-purple-900 to-pink-900 relative overflow-hidden">
       <div className="flex min-h-screen relative z-10">
         {/* Sidebar */}
-        <Sidebar />
+        <Sidebar
+          sidebar={sidebar}
+          setSidebarOpen={setSidebarOpen}
+          activeTab={activeTab}
+          setActiveTab={setActiveTab}
+        />
       </div>
       App
     </div>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,10 @@
 import { Crown, Rocket, Sparkle, X } from "lucide-react";
+import { menuItems } from "../data/data";
 
-const Sidebar = () => {
+const Sidebar = ({ sidebar, setSidebarOpen, activeTab, setActiveTab }) => {
   return (
     <div
-      className={`fixed inset-y-0 left-0 z-50 w-80 bg-white/10 backdrop-blur-2xl border-r border-white/20 transform transform-all duration-500 ease-out lg:translate-x-0 lg:static lg:inset-0`}
+      className={`${sidebar ? "translate-x-0" : "-translate-x-full"} fixed inset-y-0 left-0 z-50 w-80 bg-white/10 backdrop-blur-2xl border-r border-white/20 transform transform-all duration-500 ease-out lg:translate-x-0 lg:static lg:inset-0`}
     >
       <div className="flex intems-center justify-between h-20 px-8 border-b border-white/20">
         <div className="flex items-center space-x-4">
@@ -19,22 +20,29 @@ const Sidebar = () => {
             <p className="text-sm text-white/60 font-medium">PREMIUM SUITE</p>
           </div>
         </div>
-        <button className="lg:hidden text-white/70 hover:text-white p-2 rounded-xl hover:bg-white/10 transition-colors">
+        <button
+          className="lg:hidden text-white/70 hover:text-white p-2 rounded-xl hover:bg-white/10 transition-colors"
+          onClick={() => setSidebarOpen(false)}
+        >
           <X className="h-6 w-6" />
         </button>
       </div>
       <nav className="mt-8 px-6 space-y-2">
         {/* Menu items will go here using Map method */}
-        <button className="w-full flex items-center px-6 py-4 text-left rounded-2xl transition-all duration-300 group relative overflow-hidden">
-          {/* Conditional Rendering*/}
-          {/* <div className={`absolute inset-0 bg-gradient-to-r opacity-30 rounded-2xl`}></div> */}
-          <div
-            className={`p-2 rounded-xl bg-gradient-to-r mr-4 group-hover:scale-110 transition-all duration-300 relative z-10`}
-          >
-            Item Icon
-          </div>
-          <span className="font-semibold relative z-10">Item Label</span>
-        </button>
+        {menuItems.map((item) => {
+          return (
+            <button className="w-full flex items-center px-6 py-4 text-left rounded-2xl transition-all duration-300 group relative overflow-hidden">
+              {/* Conditional Rendering*/}
+              {/* <div className={`absolute inset-0 bg-gradient-to-r opacity-30 rounded-2xl`}></div> */}
+              <div
+                className={`p-2 rounded-xl bg-gradient-to-r mr-4 group-hover:scale-110 transition-all duration-300 relative z-10`}
+              >
+                Item Icon
+              </div>
+              <span className="font-semibold relative z-10">Item Label</span>
+            </button>
+          );
+        })}
       </nav>
       <div className="absolute bottom-0 left-0 right-0 p-6">
         <div className="relative overflow-hidden bg-gradient-to-r from-purple-500/20 to-pink-500/20 backdrop-blur-sm rounded-3xl p-6 border border-white/20">
